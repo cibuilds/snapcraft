2b21a78fc28928798933b19255f8e1fa707276cc
Ricardo N Feliciano <FelicianoTech@gmail.com>
2019-10-13 16:40:23 -0400

    Improve CI publish process. (#12)

--- build-images.sh
+++ build-images.sh
@@ -2,3 +2,5 @@
 
 docker build --file core16/Dockerfile -t cibuilds/snapcraft:core16 .
 docker build --file core18/Dockerfile -t cibuilds/snapcraft:core18 .
+
+docker tag cibuilds/snapcraft:core16 cibuilds/snapcraft:core

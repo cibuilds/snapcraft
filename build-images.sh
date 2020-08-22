440a3cfa90395c24d407f76456bec1b0574a60d0
Ricardo N Feliciano <FelicianoTech@gmail.com>
2020-08-22 13:00:39 -0400

    Add core20 support.

--- build-images.sh
+++ build-images.sh
@@ -2,5 +2,6 @@
 
 docker build --file core16/Dockerfile -t cibuilds/snapcraft:core16 .
 docker build --file core18/Dockerfile -t cibuilds/snapcraft:core18 .
+docker build --file core20/Dockerfile -t cibuilds/snapcraft:core20 .
 
 docker tag cibuilds/snapcraft:core16 cibuilds/snapcraft:core

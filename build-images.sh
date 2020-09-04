5db38fcb67b397b5f4d628a9bc359751962c5aba
Ricardo N Feliciano <FelicianoTech@gmail.com>
2020-09-04 12:09:31 -0400

    Exit on error.

--- build-images.sh
+++ build-images.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# exit immediately on error
+set -e
+
 docker build --file core16/Dockerfile -t cibuilds/snapcraft:core16 .
 docker build --file core18/Dockerfile -t cibuilds/snapcraft:core18 .
 docker build --file core20/Dockerfile -t cibuilds/snapcraft:core20 .

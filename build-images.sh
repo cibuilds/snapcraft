1dee5678d12db978c7d2c7b9ca7e673d55afda9a
Ricardo N Feliciano <FelicianoTech@gmail.com>
2021-06-14 11:34:11 -0400

    Remove core/core16, which is deprecated (#31)

--- build-images.sh
+++ build-images.sh
@@ -3,8 +3,5 @@
 # exit immediately on error
 set -e
 
-docker build --file core16/Dockerfile -t cibuilds/snapcraft:core16 .
 docker build --file core18/Dockerfile -t cibuilds/snapcraft:core18 .
 docker build --file core20/Dockerfile -t cibuilds/snapcraft:core20 .
-
-docker tag cibuilds/snapcraft:core16 cibuilds/snapcraft:core
